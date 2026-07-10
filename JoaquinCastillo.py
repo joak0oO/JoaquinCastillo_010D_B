53d3ec72abe6bc56502fee83274db2268d915b05
AV-Alumno <AV-Alumno@duoc.acad>
2026-07-10 11:01:53 -0400

    quinto commit

--- JoaquinCastillo.py
+++ JoaquinCastillo.py
@@ -83,11 +83,28 @@ def eliminar_pelicula(codigo, peliculas, cartelera):
     return False
 def mostrar_menu():
     print("\n ================== MENU PRINCIPAL ====================")
-    print("1. cupos por genero ")
-    print("2. busqueda de peliculas por rango de precio ")
-    print("3. actualizar precio de pelicula")
-    print("4. agregar pelicula")
-    print("5. eliminar pelicula")
-    print("6. salir")
+    print("1. Cupos por genero ")
+    print("2. Busqueda de peliculas por rango de precio ")
+    print("3. Actualizar precio de pelicula")
+    print("4. Agregar pelicula")
+    print("5. Eliminar pelicula")
+    print("6. Salir")
     print("=========================================================")
+    def main():
+    peliculas = {
+        'P101': ['Luz de Otoño', 'drama', 110, 'B', 'Español', False],
+        'P102': ['Noche Neón', 'acción', 125, 'C', 'Ingles', True],
+        'P103': ['Planeta Agua', 'documental', 90, 'A', 'Español', False],
+        'P104': ['Risa Total', 'comedia', 105, 'A', 'Español', True],
+        'P105': ['Código Zero', 'thriller', 118, 'C', 'Ingles', True],
+        'P106': ['Viaje Lunar', 'ciencia ficción', 132, 'B', 'Ingles', False]
+    }
     
+    cartelera = {
+        'P101': [5990, 40],
+        'P102': [7990, 0],
+        'P103': [4990, 25],
+        'P104': [6990, 12],
+        'P105': [8990, 8],
+        'P106': [7490, 3]
+    }
