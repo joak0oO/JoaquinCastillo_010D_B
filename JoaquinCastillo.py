b79210f7d0370303a3b797560b19fd97dba1574f
AV-Alumno <AV-Alumno@duoc.acad>
2026-07-10 10:21:27 -0400

    tercer commit

--- JoaquinCastillo.py
+++ JoaquinCastillo.py
@@ -20,4 +20,24 @@ def busqueda_precio(p_min, p_max, peliculas, cartelera):
     encontradas = []
     for cod, datos in cartelera.items():
         precio_datos = [0]
-        
+        cupos = datos[1]
+        if p_min <= precio <= p_max and cupos > 0:
+            if cod in peliculas:
+                titulo = peliculas[cod][0]
+                encontradas.append(f"{titulo}--{cod}")
+
+    if encontradas:
+        encontradas.sort()
+        print(f"Las películas encontradas son: {encontradas}")
+    else:
+        print("No hay películas en ese rango de precios.")
+
+def buscar_codigo(codigo, cartelera):
+    return codigo.upper() in cartelera
+
+def actualizar_precio(codigo, nuevo_precio, cartelera):
+    cod_upper = codigo.upper()
+    if buscar_codigo(cod_upper, cartelera):
+        cartelera[cod_upper][0] = nuevo_precio
+        return True
+    return False
