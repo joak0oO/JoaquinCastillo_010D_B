8ae8706d28590587fa4822a4604257a7e0da636f
AV-Alumno <AV-Alumno@duoc.acad>
2026-07-10 10:18:38 -0400

    segundo commit

---
+++ JoaquinCastillo.py
@@ -0,0 +1,23 @@
+def leer_opcion()
+    while true:
+        try:
+            opcion = int(input("Ingrese una opcion: "))
+            if 1 <= opcion <= 6:
+                return opcion
+            else:
+                print("Debe  seleccionar una opcion valida")
+        except ValueError:
+            print("Debe seleccionar una opcion valida")
+def cupos_genero (genero, peliculas, cartelera):
+    total_cupos = 0
+    genero_buscado = genero_strip().lower()
+    for cod, datos in peliculas_items():
+        if datos[1].strip().lower() == genero_buscado:
+            if cod in cartelera:
+                total_cupos += cartelera[cod][1]
+    print(f"El total de cupos disponibles es: {total_cupos}")
+def busqueda_precio(p_min, p_max, peliculas, cartelera):
+    encontradas = []
+    for cod, datos in cartelera.items():
+        precio_datos = [0]
+        
